6af84785f4db775c92f121438f31056d0fe618c7
Raphael Bernoldi <raphaelbernoldi@gmail.com>
2020-05-14 20:48:12 -0300

    Instacalao e brincada com client

---
+++ kafka/cola.sh
@@ -0,0 +1,27 @@
+#Instalation
+#Downlaod 
+#https://www.apache.org/dyn/closer.cgi?path=/kafka/2.5.0/kafka_2.12-2.5.0.tgz
+
+#Documentation
+#http://kafka.apache.org/quickstart
+
+tar -xzf kafka_2.12-2.5.0.tgz
+
+#commands_utils
+#start
+./bin/zookeeper-server-start.sh config/zookeeper.properties
+./bin/kafka-server-start.sh config/server.properties 
+
+#Lista topicos
+./kafka-topics.sh --list --bootstrap-server localhost:9092
+
+#cria topicos
+./kafka-topics.sh --bootstrap-server localhost:9092 --create --topic NOME_TOPIC
+
+#Cria mensagem em um topico
+./kafka-console-producer.sh --bootstrap-server localhost:9092 --topic LOJA_NOVO_PEDIDO
+
+
+#Consome mensagem de um topico
+./kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic LOJA_NOVO_PEDIDO --from-beginning
+
