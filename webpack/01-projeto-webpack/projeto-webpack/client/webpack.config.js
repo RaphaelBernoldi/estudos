4d19c010dfb62ac460e5964c8d65f365b0abea35
Raphael Bernoldi <raphaelbernoldi@gmail.com>
2020-05-25 14:57:14 -0300

    Minificando bundle

--- webpack/01-projeto-webpack/projeto-webpack/client/webpack.config.js
+++ webpack/01-projeto-webpack/projeto-webpack/client/webpack.config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 
+// Também seria uma boa soulução para criar arquivo minificado em prod:
+// webpack  -p --config webpack.config.js
+
 module.exports = {
     entry: './app-src/app.js',
-    mode: 'development',
+    mode: process.env.NODE_ENV == null ? 'development' : process.env.NODE_ENV,
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname,'dist')
