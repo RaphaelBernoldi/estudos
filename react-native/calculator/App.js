7b599dd9a27465cc3d129927eeb89959f949e3f3
Raphael Bernoldi <raphaelbernoldi@gmail.com>
2020-06-21 13:34:43 -0300

    Implementacao de logica da calculadora

--- react-native/calculator/App.js
+++ react-native/calculator/App.js
@@ -1,29 +1,126 @@
 import React, { Component } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  View, StyleSheet
 } from 'react-native';
 
-import {
-  Header, 
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,  
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Button from './src/component/Button';
+import Display from './src/component/Display';
 
 
 export default class App extends Component {
 
+  state = {
+    displayValue: '0',
+    n1: 0,
+    n2: 0,
+    operacao: ''
+  }
+
+  addDigit = n => {
+    if( this.state.displayValue === '0'){
+      this.setState({displayValue: n, n1: n})
+      
+
+    }else {
+
+      if(this.state.operacao === ''){
+        this.setState({displayValue: this.state.displayValue + n
+                     , n1: this.state.displayValue + n})
+
+      }else {
+        if(this.state.n2 === 0){
+          this.setState({displayValue: n ,n2: n})
+
+        } else {
+          this.setState({displayValue: this.state.displayValue + n
+                       , n2: this.state.displayValue + n})
+        }
+        
+      }
+      
+    }
+  }
+
+  calcula = () => {
+
+    if(this.state.operacao != '' && this.state.n1 != '0' && this.state.n2 != '0'){
+
+      var n1 = Number(this.state.n1);
+      var n2 = Number(this.state.n2);
+
+      if(this.state.operacao === '+'){
+        const resultado = n1 + n2
+        this.setState({displayValue: resultado
+                    , n1: resultado
+                    , n2: 0})
+      }
+      if(this.state.operacao === '-'){
+        const resultado = n1 - n2
+        this.setState({displayValue: resultado
+                    , n1: resultado
+                    , n2: 0})
+      }
+      if(this.state.operacao === '*'){
+        const resultado = n1 * n2
+        this.setState({displayValue: resultado
+                    , n1:  resultado
+                    , n2: 0})
+      }
+      if(this.state.operacao === '/'){
+        const resultado = n1 / n2
+        this.setState({displayValue: resultado
+                     , n1: resultado
+                     , n2: 0})
+      }
+    }
+  }
+
+  addOperation = op => {
+
+    if( this.state.displayValue !== '0'){
+
+      if(!!this.state.operacao){
+        this.setState({ operacao: op })
+        this.calcula()
+
+      } else {
+        this.setState({ operacao: op })
+      }
+      
+    }
+  }
+
+  clear = () => {
+    this.setState({displayValue: '0'
+                 , n1: 0
+                 , n2: 0
+                 , operacao: ''})
+  }
+
   render(){
     return(
       <View style={styles.container}>
-        <Text style={styles.welcome}>welcome</Text>
-        <Text style={styles.instruction}>App.js</Text>
+        <Display value={this.state.displayValue}></Display>
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={this.clear}/>
+          <Button label='/' operation onClick={this.addOperation}/>
+          <Button label='7' onClick={this.addDigit} />
+          <Button label='8' onClick={this.addDigit} />
+          <Button label='9' onClick={this.addDigit}/>
+          <Button label='*' operation onClick={this.addOperation}/>
+          <Button label='4' onClick={this.addDigit} />
+          <Button label='5' onClick={this.addDigit}/>
+          <Button label='6' onClick={this.addDigit}/>
+          <Button label='-' operation onClick={this.addOperation} />
+          <Button label='1' onClick={this.addDigit}/>
+          <Button label='2' onClick={this.addDigit}/>
+          <Button label='3' onClick={this.addDigit}/>
+          <Button label='+' operation onClick={this.addOperation}/>
+          <Button label='0' double onClick={this.addDigit}/>
+          <Button label='.' onClick={this.addDigit} />
+          <Button label='=' operation onClick={this.calcula}/>
+        </View>
+        
       </View>
     )
   }
@@ -31,19 +128,11 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF' 
+    flex: 1
   },  
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instruction: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+
   }
 });
